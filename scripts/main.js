7e34ec3deec25f24716050456d95e6aec155102e
cmjoh <54944900+cmjoh@users.noreply.github.com>
2019-10-03 08:55:27 -0500

    added som java

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,25 @@
-// Put your JavaScript here
-plz work
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+  let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src' , imageUrl);
+
+  let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
+
+function showDetails() {
+  'use strict';
+  let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  },50);
+}
