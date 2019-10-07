54e6eeda451866ed37444d5347c90d8d8a50898d
cmjoh <54944900+cmjoh@users.noreply.github.com>
2019-10-07 08:55:49 -0500

    did a lot of java, and now i am done. might add more tho

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,53 @@ const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-const HIDDEN_DETAIL_CLASS = 'hidden-detail';
-const TINY_EFFECT_CLASS = 'is-tiny';
-const ESC_KEY_CODE = 27;
+
 
 function setDetails(imageUrl, titleText) {
   'use strict';
+  let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src' , imageUrl);
+
+  let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
 }
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickerHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+  })
+}
+
+function matchElements() {
+  'use strict';
+  let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  let thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+function initializeEvents() {
+  'use strict';
+  let thumbnails = matchElements();
+  thumbnails.forEach(addThumbClickerHandler);
+}
+
+initializeEvents();
